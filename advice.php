5e5a7e102a99144ee04fa330dbdd7e86b6e04f17
Cody Whitby <cody@codywhitby.net>
2014-04-26 12:38:29 -0400

    nonsense

--- advice.php
+++ advice.php
@@ -3,7 +3,6 @@
 Template Name: Advice
 */
 ?>
-
 <div id="container">
   <div class="row">
     <div id="content" role="main" class="col-md-7">
@@ -22,13 +21,21 @@ Template Name: Advice
           $advicequery->the_post(); ?>
           <div class="advice-post">
             <?php
-            if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-              the_post_thumbnail('large', array( 'class'  => "img-responsive"));
-            }
+            if ( has_post_thumbnail() ) { ?>
+              <a href="<?php echo get_permalink(); ?>"> <?php the_post_thumbnail('large', array( 'class'  => "img-responsive advice-img")); ?> </a>
+            <?php }
             ?>
-            <h4 class="brown-pagetitle advice-title"><?php the_title(); ?></h4>
-            <small class="advice-postedon">posted <?php the_time('l, F jS, Y') ?></small>
-            <div class="advice-post-content"><?php the_content(); ?></div>
+            <h4 class="brown-pagetitle advice-title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h4>
+            <?php get_template_part('templates/entry-meta'); ?>
+            <?php global $more; $more = 0; ?>
+            <div class="advice-post-content"><?php the_content(''); ?></div>
+            <section class="advice-social">
+              <span class="brown-pagetitle"><strong>Share:</strong></span>
+              <a target="_blank" href="https://plus.google.com/share?url=<?php echo get_permalink(); ?>" class="social-icon"><i class="fa fa-google-plus"></i></a>
+              <a target="_blank" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?> &url=<?php echo get_permalink(); ?>&via=TheBeranGroup" class="social-icon"><i class="fa fa-twitter"></i></a>
+              <a target="_blank" href="https://pinterest.com/pin/create/button/?url=<?php echo get_permalink(); ?>&description=A blog dedicated to all things related to the nest&media=<?php echo get_theme_mod( 'beran_logo_upload' ); ?>" class="social-icon"><i class="fa fa-pinterest"></i></a>
+              <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>" class="social-icon"><i class="fa fa-facebook"></i></a>
+            </section>
           </div>
         <?php }
       } else { ?>
@@ -50,11 +57,46 @@ Template Name: Advice
         </nav>
       <?php endif; ?>
     </div><!-- #content -->
-    <div class="col-md-5">
+    <div class="advice-sidebar col-md-5">
+      <section class="advice-social">
+        <span class="brown-pagetitle"><strong>Share:</strong></span>
+        <a target="_blank" href="https://plus.google.com/share?url=<?php echo home_url('/advice') ?>" class="social-icon"><i class="fa fa-google-plus"></i></a>
+        <a target="_blank" href="https://twitter.com/intent/tweet?text=A blog dedicated to all things related to the nest&url=<?php echo home_url('/advice') ?>&via=TheBeranGroup" class="social-icon"><i class="fa fa-twitter"></i></a>
+        <a target="_blank" href="https://pinterest.com/pin/create/button/?url=<?php echo home_url('/advice') ?>&description=A blog dedicated to all things related to the nest&media=<?php echo get_theme_mod( 'beran_logo_upload' ); ?>" class="social-icon"><i class="fa fa-pinterest"></i></a>
+        <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo home_url('/advice') ?>" class="social-icon"><i class="fa fa-facebook"></i></a>
+      </section>
+      <h4 class="beran-headline">Subscribe</h4>
+      <div class="newsletter-form">
+        <h4>Sign up for up-to-the-minute listings, news and advice.</h4>
+        <div id="mc_embed_signup">
+          <form action="http://theberangroup.us8.list-manage.com/subscribe/post?u=cc81e067058b767efb1fb26be&amp;id=e39baaebbb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" role="form">
+            <div class="form-group">
+              <input id="mce-EMAIL" type="email" class="form-control required email" name="EMAIL" placeholder="Enter Email Address">
+              <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+              </div>
+            </div>
+            <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-newsletter"><span class="glyphicon glyphicon-chevron-right"></span></button>
+            <div style="position: absolute; left: -5000px;"><input type="text" name="b_cc81e067058b767efb1fb26be_e39baaebbb" value=""></div>
+          </form>
+        </div>
+      </div>
+      <h4 class="beran-headline">Recent Posts</h4>
+      <ul class="advice-recentposts">
+        <?php
+          $args = array( 'numberposts' => '3' );
+          $recent_posts = wp_get_recent_posts( $args );
+          foreach( $recent_posts as $recent ){
+            echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
+          }
+        ?>
+      </ul>
       <h4 class="beran-headline">Archives</h4>
       <ul class="archives-list list-unstyled">
         <?php wp_get_archives('type=monthly'); ?>
       </ul>
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/birdnest.png" class="birdnest advice-nest">
     </div>
   </div>
 </div><!-- #container -->
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,15 +1,68 @@
 <?php while (have_posts()) : the_post(); ?>
+<div id="content" role="main" class="col-md-7">
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <h2 class="beran-headline"><?php the_title(); ?></h2>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
-    <div class="entry-content">
+    <div class="entry-content advice-post">
+      <?php
+        if ( has_post_thumbnail() ) { ?>
+          <a href="<?php echo get_permalink(); ?>"> <?php the_post_thumbnail('large', array( 'class'  => "img-responsive advice-img")); ?> </a>
+        <?php }
+      ?>
       <?php the_content(); ?>
+      <section class="advice-social">
+        <span class="brown-pagetitle"><strong>Share:</strong></span>
+        <a target="_blank" href="https://plus.google.com/share?url=<?php echo get_permalink(); ?>" class="social-icon"><i class="fa fa-google-plus"></i></a>
+        <a target="_blank" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&url=<?php echo get_permalink(); ?>&via=TheBeranGroup" class="social-icon"><i class="fa fa-twitter"></i></a>
+        <a target="_blank" href="https://pinterest.com/pin/create/button/?url=<?php echo get_permalink(); ?>&description=<?php the_title(); ?>&media=<?php echo get_theme_mod( 'beran_logo_upload' ); ?>" class="social-icon"><i class="fa fa-pinterest"></i></a>
+        <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>" class="social-icon"><i class="fa fa-facebook"></i></a>
+      </section>
     </div>
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
     </footer>
-    <?php comments_template('/templates/comments.php'); ?>
   </article>
+</div>
+<div class="advice-sidebar col-md-5">
+  <section class="advice-social visible-lg visible-md">
+    <span class="brown-pagetitle"><strong>Share:</strong></span>
+    <a target="_blank" href="https://plus.google.com/share?url=<?php echo get_permalink(); ?>" class="social-icon"><i class="fa fa-google-plus"></i></a>
+    <a target="_blank" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&url=<?php echo get_permalink(); ?>&via=TheBeranGroup" class="social-icon"><i class="fa fa-twitter"></i></a>
+    <a target="_blank" href="https://pinterest.com/pin/create/button/?url=<?php echo get_permalink(); ?>&description=<?php the_title(); ?>&media=<?php echo get_theme_mod( 'beran_logo_upload' ); ?>" class="social-icon"><i class="fa fa-pinterest"></i></a>
+    <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>" class="social-icon"><i class="fa fa-facebook"></i></a>
+  </section>
+  <h4 class="beran-headline">Subscribe</h4>
+  <div class="newsletter-form">
+    <h4>Sign up for up-to-the-minute listings, news and advice.</h4>
+    <div id="mc_embed_signup">
+      <form action="http://theberangroup.us8.list-manage.com/subscribe/post?u=cc81e067058b767efb1fb26be&amp;id=e39baaebbb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" role="form">
+        <div class="form-group">
+          <input id="mce-EMAIL" type="email" class="form-control required email" name="EMAIL" placeholder="Enter Email Address">
+          <div id="mce-responses" class="clear">
+            <div class="response" id="mce-error-response" style="display:none"></div>
+            <div class="response" id="mce-success-response" style="display:none"></div>
+          </div>
+        </div>
+        <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-newsletter"><span class="glyphicon glyphicon-chevron-right"></span></button>
+        <div style="position: absolute; left: -5000px;"><input type="text" name="b_cc81e067058b767efb1fb26be_e39baaebbb" value=""></div>
+      </form>
+    </div>
+  </div>
+  <h4 class="beran-headline">Recent Posts</h4>
+  <ul class="advice-recentposts">
+    <?php
+      $args = array( 'numberposts' => '3' );
+      $recent_posts = wp_get_recent_posts( $args );
+      foreach( $recent_posts as $recent ){
+        echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
+      } ?>
+  </ul>
+  <h4 class="beran-headline">Archives</h4>
+  <ul class="archives-list list-unstyled">
+    <?php wp_get_archives('type=monthly'); ?>
+  </ul>
+  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/birdnest.png" class="birdnest">
+</div>
 <?php endwhile; ?>
